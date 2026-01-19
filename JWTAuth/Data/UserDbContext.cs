3afa8c1483d3b858471ea592423b068476d2a829
Tawfik <Tawfik@DESKTOP-7I660Q0>
2026-01-19 20:14:57 +0100

    Add project files.

---
+++ JWTAuth/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JWTAuth.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuth.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext (options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ JWTAuth/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using JWTAuth.DTO;
+using JWTAuth.Entities;
+
+namespace JWTAuth.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDto request);
+        Task<string?> LoginAsync(UserDto request);
+    }
+}
